51b105ac6b705d917685cca9f2b6669414e2dc00
scubarudy-1 <steven.shotts@gmail.com>
2026-01-11 08:21:40 -0600

    Delete src/app/api/daily directory

--- src/app/api/daily/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from "next/server";
-import { getDailyWord } from "../../../utils/dailyWord";
-
-export async function GET() {
-  return NextResponse.json({ word: getDailyWord().toUpperCase() });
-}
