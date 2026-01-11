ab4bae57bd825666e389e4bd184560857094d222
scubarudy-1 <steven.shotts@gmail.com>
2026-01-11 08:45:27 -0600

    removing debug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,5 @@ import { getDailyWord } from "../utils/dailyWord";
 export default function Page() {
   const dailyWord = getDailyWord();
 
-  return (
-    <>
-      <p style={{ padding: "1rem", fontFamily: "Arial" }}>
-        <em>SERVER dailyWord = {dailyWord}</em>
-      </p>
-      <Game dailyWord={dailyWord.toUpperCase()} />
-    </>
-  );
+  return <Game dailyWord={dailyWord.toUpperCase()} />;
 }
